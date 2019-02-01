f7084aaf47cd37e941c80c4af6a036650d62cbe1
JP <1.4.gigz@gmail.com>
2019-01-31 20:28:21 -0600

    Add User login functionality. Closes #14.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+
+// Load User model
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users GET route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: 'User works'}));
+
+// @route   POST api/users/register
+// @desc    Register User
+// @access  Public 
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+  .then(user => {
+    if(user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        size: '200',
+        rating: 'pg',
+        default: 'mm' 
+      });
+
+      const newUser = new User({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  })
+});
 
 module.exports = router;
