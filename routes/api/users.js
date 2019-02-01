1ebfc1ac1ccc610ebcba5a51b24fb4a0fdea71d3
JP <1.4.gigz@gmail.com>
2019-01-31 20:55:05 -0600

    Encrypt User password with hash. Closes #15.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,28 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Tests users GET route
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: 'User works'}));
+router.get("/test", (req, res) => res.json({ msg: "User works" }));
 
 // @route   POST api/users/register
 // @desc    Register User
-// @access  Public 
-router.post('/register', (req, res) => {
-  User.findOne({ email: req.body.email })
-  .then(user => {
-    if(user) {
-      return res.status(400).json({ email: 'Email already exists' });
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
     } else {
       const avatar = gravatar.url(req.body.email, {
-        size: '200',
-        rating: 'pg',
-        default: 'mm' 
+        size: "200",
+        rating: "pg",
+        default: "mm"
       });
 
       const newUser = new User({
@@ -32,8 +32,19 @@ router.post('/register', (req, res) => {
         avatar,
         password: req.body.password
       });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
     }
-  })
+  });
 });
 
 module.exports = router;
